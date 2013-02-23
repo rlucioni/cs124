54d7001b6b8f784e812ab5436119e8e918d75e79
Renzo Lucioni <renzolucioni@gmail.com>
2013-02-23 17:22:50 -0500

    created new file to hold kruskal's implementation and edge sum function

---
+++ assignment1/kruskals.c
@@ -0,0 +1,28 @@
+#include "graph.h"
+
+// Kruskal's returns an adjacency matrix of the MST, indexed by node
+
+graph* kruskals(input_graph*) {
+    // initialize new graph - empty
+    graph* output_graph;
+
+    // sort edges of input_graph by weight
+
+    // for each vertex in input_graph, makeset containing vertex
+    
+    // cycle through edges in input_graph
+
+        // if endpoints not in the same vertex set, union the edge with edges in output_graph
+        
+        // union the endpoint sets
+        
+    return output_graph;
+}
+
+graph* sum_edges(input_graph*) {
+    int sum = 0;
+
+    // loop through edges in input_graph, add to sum
+
+    return sum;
+}
