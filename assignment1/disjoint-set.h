ad617641e0abbac79ccfff5f90791c92329ff531
msheets <sheetmat@gmail.com>
2013-02-23 17:52:38 -0500

    disjoint-set.h

--- assignment1/disjoint-set.h
+++ assignment1/disjoint-set.h
@@ -1,3 +1,17 @@
+#include <stdbool.h>
+
+typedef struct forest {
+    void *;
+} forest;
+
+forest *make_forest();
+void burn_forest(forest *);
+
+forest *make_set(void *x);
+
+forest *unionize(void *x, void *y);
+
+void *find(void *x);
 
 
 
