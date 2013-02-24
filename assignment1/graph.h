cbc28c86103bd0a867e56511813ae93f51783d52
msheets <sheetmat@gmail.com>
2013-02-24 17:45:43 -0500

    new graph.h

--- assignment1/graph.h
+++ assignment1/graph.h
@@ -1,6 +1,13 @@
 typedef struct {
-    float **adj_matrix;
+    int u;
+    int v;
+    float weight;
+} edge;
+
+typedef struct {
+    edge *list;
     int num_nodes;
+    int num_edges;
 } graph;
 
 typedef struct {
@@ -14,13 +21,6 @@ typedef struct {
 //    specification
 graph *graph_generate(int dimensions, int numpoints);
 void graph_free(graph *g);
-int graph_size(graph *g);
-
-// get_edge(n1, n2)
-//    Returns the weight of the edge between n1 and n2
-//    If no edge exists between n1 and n2, get_edge returns
-//    -1.
-float graph_get_edge(graph *g, int n1, int n2);
 
 int graph_run_tests();
 test_graph *graph_test_graphs();
