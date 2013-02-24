c19825ce8d8e0e3be4f2a204511e914724604d8e
msheets <sheetmat@gmail.com>
2013-02-24 00:05:27 -0500

    graph done

--- assignment1/graph.c
+++ assignment1/graph.c
@@ -1,1 +1,84 @@
-#import "graph.h"
+#include "graph.h"
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define RAND_FLOAT() (rand() / RAND_MAX)
+#define SQUARE(x) ((x) * (x))
+
+graph *empty_graph(int numpoints) {
+    graph *g = (graph *) malloc(sizeof(graph));
+    g->adj_matrix = (float **) malloc(sizeof(float *) * numpoints);
+    for (int i = 0; i < numpoints; ++i) {
+	g->adj_matrix[i] = (float *) malloc(sizeof(float) * numpoints);
+	memset(g->adj_matrix[i], -1, sizeof(float) * numpoints);
+    }
+    g->num_nodes = numpoints;
+
+    return g;
+}
+
+void free_graph(graph *g) {
+    for (i = 0; i < g->numpoints; ++g)
+	free(g->adj_matrix[i]);
+    free(g->adj_matrix);
+    free(g);
+}
+
+graph *generate_graph0(int numpoints) {
+    graph *g = empty_graph(numpoints);
+    for (int i = 0; i < numpoints; ++i) {
+	for (int j = 0; j < numpoints; ++j) {
+	    g->adj_matrix[i][j] = RAND_FLOAT();
+	}
+    }
+    
+    return g;
+}
+
+graph *generate_euclidean(int dimensions, int numpoints) {
+    graph *g = empty_graph(numpoints);
+    float ps[numpoints][dimensions]; // array of points, each of dimensions dimensions
+    for (int i = 0; i < numpoints; ++i) {
+	for (int d = 0; d < dimensions; ++d) {
+	    ps[i][d] = RAND_FLOAT();
+	}
+    }
+    for (int i = 0; i < numpoints; ++i) {
+	for (int j = 0; j < numpoints; ++j) {
+	    float distance = 0;
+	    for (int d = 0; d < dimensions; ++d) {
+		distance += SQUARE(ps[i][d] - ps[j][d]);
+	    }
+	    g->adj_matrix[i][j] = sqrt(distance);
+	}
+    }
+
+    return g;
+}
+
+graph *generate_graph(int dimensions, int numpoints) {
+    // seed random number generator
+    srand(time(NULL));
+    
+    // call appropriate graph generation function
+    switch (dimensions) {
+    case 0: return generate_graph0(numpoints);
+    case 2: 
+    case 3: 
+    case 4: return generate_euclidean(dimensions, numpoints);
+    default: return NULL;
+    }
+}
+
+float get_edge(graph *g, int n1, int n2) {
+    return g->adj_matrix[n1][n2];
+}
+
+void add_edge(graph *g, int n1, int n2, float weight) {
+    g->adj_matrix[n1][n2] = weight;
+}
+
+void delete_edge(graph *g, int n1, int n2) {
+    g->adj_matrix[n1][n2] = -1;
+}
--- assignment1/graph.h
+++ assignment1/graph.h
@@ -1,9 +1,12 @@
-typedef graph **float;
-typedef node int;
+typedef struct {
+    float **adj_matrix;
+    int num_nodes;
+} graph;
 
 // empty_graph()
 //    Returns a pointer to an empty graph
-graph *empty_graph();
+graph *empty_graph(int numpoints);
+void free_graph(graph *g);
 
 // generate_graph(dimensions, numpoints)
 //    Returns a pointer to a graph of w/ numpoints vertices (nodes). 
@@ -15,8 +18,7 @@ graph *generate_graph(int dimensions, int numpoints);
 //    Returns the weight of the edge between n1 and n2
 //    If no edge exists between n1 and n2, get_edge returns
 //    -1.
-float get_edge(node *n1, node *n2);
-
-void add_edge(graph *g, node *n1, node *n2);
-void delete_edge(graph *g, node *n1, node *n2);
+float get_edge(graph *g, int n1, int n2);
+void add_edge(graph *g, int n1, int n2, float weight);
+void delete_edge(graph *g, int n1, int n2);
 
