09dda95c77457ff76da6b9eb48c1534937abddc4
msheets <sheetmat@gmail.com>
2013-02-24 00:40:13 -0500

    some minor updates to make kruskals runnable

--- assignment1/kruskals.c
+++ assignment1/kruskals.c
@@ -1,10 +1,11 @@
 #include "graph.h"
+#include <stdio.h>
 
 // Kruskal's returns an adjacency matrix of the MST, indexed by node
 
-graph* kruskals(input_graph*) {
+float kruskals(graph *input_graph) {
     // initialize new graph - empty
-    graph* output_graph;
+    float mst_weight = 0;
 
     // sort edges of input_graph by weight
 
@@ -16,7 +17,7 @@ graph* kruskals(input_graph*) {
         
         // union the endpoint sets
         
-    return output_graph;
+    return mst_weight;
 }
 
 graph* sum_edges(input_graph*) {
@@ -25,4 +26,27 @@ graph* sum_edges(input_graph*) {
     // loop through edges in input_graph, add to sum
 
     return sum;
+}
+
+int main(int argc, char **argv) {
+    // process command line arguments
+    if (argc != 5) {
+	fprintf(stderr, "usage: %s flag numpoints numtrials dimension\n", argv[0]);
+	exit(1);
+    }
+    int flag = atoi(argv[1]);
+    int numpoints = atoi(argv[2]);
+    int numtrials = atoi(argv[3]);
+    int dimension = atoi(argv[4]);
+    
+    // run trials
+    float running_total = 0;
+    for (int i = 0; i < numtrials; ++i) {
+	graph *g = generate_graph(dimension, numpoints);
+	running_total += kruskals(g);
+	free_graph(g);
+    }
+
+    printf("%f %d %d %d\n", running_total / numpoints, numpoints, numtrials, dimension);
+    return 0;
 }
