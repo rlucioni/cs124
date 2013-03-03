c210bdfa38e00845aa820d41e8f9d16b40151453
Renzo Lucioni <renzolucioni@gmail.com>
2013-03-02 22:46:42 -0500

    more points in the weight script

--- assignment1/weight_script.sh
+++ assignment1/weight_script.sh
@@ -5,21 +5,39 @@ rm weight0.txt weight2.txt weight3.txt weight4.txt
 echo "RUNNING KRUSKALS, 0 DIMENSIONS..."
 ./randmst 3 16 5 0 >> weight0.txt
 wait
+./randmst 3 25 5 0 >> weight0.txt
+wait
 ./randmst 3 32 5 0 >> weight0.txt
+wait
+./randmst 3 50 5 0 >> weight0.txt
 wait
 ./randmst 3 64 5 0 >> weight0.txt
 wait
+./randmst 3 90 5 0 >> weight0.txt
+wait
 ./randmst 3 128 5 0 >> weight0.txt
+wait
+./randmst 3 190 5 0 >> weight0.txt
 wait
 ./randmst 3 256 5 0 >> weight0.txt
 wait
+./randmst 3 350 5 0 >> weight0.txt
+wait
 ./randmst 3 512 5 0 >> weight0.txt
+wait
+./randmst 3 750 5 0 >> weight0.txt
 wait
 ./randmst 3 1024 5 0 >> weight0.txt
 wait
+./randmst 3 1500 5 0 >> weight0.txt
+wait
 ./randmst 3 2048 5 0 >> weight0.txt
 wait
+./randmst 3 3000 5 0 >> weight0.txt
+wait
 ./randmst 3 4096 5 0 >> weight0.txt
+wait
+./randmst 3 6000 5 0 >> weight0.txt
 wait
 ./randmst 3 8192 5 0 >> weight0.txt
 wait
@@ -31,21 +49,39 @@ wait
 echo "RUNNING KRUSKALS, 2 DIMENSIONS..."
 ./randmst 3 16 5 2 >> weight2.txt
 wait
+./randmst 3 25 5 2 >> weight2.txt
+wait
 ./randmst 3 32 5 2 >> weight2.txt
+wait
+./randmst 3 50 5 2 >> weight2.txt
 wait
 ./randmst 3 64 5 2 >> weight2.txt
 wait
+./randmst 3 90 5 2 >> weight2.txt
+wait
 ./randmst 3 128 5 2 >> weight2.txt
+wait
+./randmst 3 190 5 2 >> weight2.txt
 wait
 ./randmst 3 256 5 2 >> weight2.txt
 wait
+./randmst 3 350 5 2 >> weight2.txt
+wait
 ./randmst 3 512 5 2 >> weight2.txt
+wait
+./randmst 3 750 5 2 >> weight2.txt
 wait
 ./randmst 3 1024 5 2 >> weight2.txt
 wait
+./randmst 3 1500 5 2 >> weight2.txt
+wait
 ./randmst 3 2048 5 2 >> weight2.txt
 wait
+./randmst 3 3000 5 2 >> weight2.txt
+wait
 ./randmst 3 4096 5 2 >> weight2.txt
+wait
+./randmst 3 6000 5 2 >> weight2.txt
 wait
 ./randmst 3 8192 5 2 >> weight2.txt
 wait
@@ -57,21 +93,39 @@ wait
 echo "RUNNING KRUSKALS, 3 DIMENSIONS..."
 ./randmst 3 16 5 3 >> weight3.txt
 wait
+./randmst 3 25 5 3 >> weight3.txt
+wait
 ./randmst 3 32 5 3 >> weight3.txt
+wait
+./randmst 3 50 5 3 >> weight3.txt
 wait
 ./randmst 3 64 5 3 >> weight3.txt
 wait
+./randmst 3 90 5 3 >> weight3.txt
+wait
 ./randmst 3 128 5 3 >> weight3.txt
+wait
+./randmst 3 190 5 3 >> weight3.txt
 wait
 ./randmst 3 256 5 3 >> weight3.txt
 wait
+./randmst 3 350 5 3 >> weight3.txt
+wait
 ./randmst 3 512 5 3 >> weight3.txt
+wait
+./randmst 3 750 5 3 >> weight3.txt
 wait
 ./randmst 3 1024 5 3 >> weight3.txt
 wait
+./randmst 3 1500 5 3 >> weight3.txt
+wait
 ./randmst 3 2048 5 3 >> weight3.txt
 wait
+./randmst 3 3000 5 3 >> weight3.txt
+wait
 ./randmst 3 4096 5 3 >> weight3.txt
+wait
+./randmst 3 6000 5 3 >> weight3.txt
 wait
 ./randmst 3 8192 5 3 >> weight3.txt
 wait
@@ -83,21 +137,39 @@ wait
 echo "RUNNING KRUSKALS, 4 DIMENSIONS..."
 ./randmst 3 16 5 4 >> weight4.txt
 wait
+./randmst 3 25 5 4 >> weight4.txt
+wait
 ./randmst 3 32 5 4 >> weight4.txt
+wait
+./randmst 3 50 5 4 >> weight4.txt
 wait
 ./randmst 3 64 5 4 >> weight4.txt
 wait
+./randmst 3 90 5 4 >> weight4.txt
+wait
 ./randmst 3 128 5 4 >> weight4.txt
+wait
+./randmst 3 190 5 4 >> weight4.txt
 wait
 ./randmst 3 256 5 4 >> weight4.txt
 wait
+./randmst 3 350 5 4 >> weight4.txt
+wait
 ./randmst 3 512 5 4 >> weight4.txt
+wait
+./randmst 3 750 5 4 >> weight4.txt
 wait
 ./randmst 3 1024 5 4 >> weight4.txt
 wait
+./randmst 3 1500 5 4 >> weight4.txt
+wait
 ./randmst 3 2048 5 4 >> weight4.txt
 wait
+./randmst 3 3000 5 4 >> weight4.txt
+wait
 ./randmst 3 4096 5 4 >> weight4.txt
+wait
+./randmst 3 6000 5 4 >> weight4.txt
 wait
 ./randmst 3 8192 5 4 >> weight4.txt
 wait
