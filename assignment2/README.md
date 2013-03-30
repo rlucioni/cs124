c989aed51e89c5957705e0e1a9784fb6f0621096
msheets <sheetmat@gmail.com>
2013-03-30 02:32:20 -0400

    coded...

--- assignment2/README.md
+++ assignment2/README.md
@@ -1,2 +1,4 @@
-Testing strassen first requires generating text files with the matrix_generator.sh script, which takes one argument: n. Once the matrices 
-have been generated, run test_script.sh, which also takes n as an argument, to run the compiled strassen executable on binary, 012, and neg matrices.
+Testing strassen first requires generating text files with the 
+matrix_generator.sh script, which takes one argument: n. Once the matrices have
+been generated, run test_script.sh, which also takes n as an argument, to run 
+the compiled strassen executable on binary, 012, and neg matrices.
--- assignment2/matrix_multiply.c
+++ assignment2/matrix_multiply.c
@@ -1,12 +1,123 @@
 #include "matrix_multiply.h"
 // accesses the element in "matrix" of dimension "dim" at "row" and "column"
 #define MELT(matrix, dim, row, col) (matrix)[(row) * (dim) + (col)]
+#define CROSSOVER 10
 
 void square_matrix_multiply(int *c, const int *a, const int *b, int dim) {
     for (int i = 0; i < dim; i++)
-        for (int k = 0; k < dim; k++)
+        for (int j = 0; j < dim; j++)
+            MELT(c, dim, i, j) = 0;
+    for (int i = 0; i < dim; i++)
+        for (int k = 0; k < dim; k++) {
+	    int a_ik = MELT(a, dim, i, k);
             for (int j = 0; j < dim; j++)
-                MELT(c, dim, i, j) += MELT(a, dim, i, k) * MELT(b, dim, k, j);
+                MELT(c, dim, i, j) += a_ik * MELT(b, dim, k, j);
+	}
+}
+
+// madd(c, a, b, dim)
+//   Addition of square matrices (a + b = c)
+void madd(int *c, const int *a, const int *b, int dim) {
+    for (int i = 0; i < dim; i++)
+	for (int j = 0; j < dim; j++)
+	    MELT(c, dim, i, j) = MELT(a, dim, i, j) + MELT(b, dim, i, j);
+}
+
+// msub(c, a, b, dim)
+//   Subtraction of square matrices (a - b = c)
+void msub(int *c, const int *a, const int *b, int dim) {
+    for (int i = 0; i < dim; i++)
+	for (int j = 0; j < dim; j++)
+	    MELT(c, dim, i, j) = MELT(a, dim, i, j) - MELT(b, dim, i, j);
+}
+
+// sadd(e, a, b, c, d, dim)
+//   Composition of several matrix operations for use by Strassen to avoid
+//   redundant loops (used for AE + BG in the Strassen Algorithm)
+// TODO: Inline to optimize away function call (since only used once)
+void sadd(int *e, const int *a, const int *b, const int *c, const int *d, 
+	  int dim) {
+    for (int i = 0; i < dim; i++)
+	for (int j = 0; j < dim; j++)
+	    MELT(e, dim, i, j) = MELT(a, dim, i, j) + MELT(b, dim, i, j)
+		- MELT(c, dim, i, j) + MELT(d, dim, i, j);
+}
+
+// ssub(e, a, b, c, d, dim)
+//   Composition of several matrix operations for use by Strassen to avoid
+//   redundant loops (used for CF + DH in the Strassen Algorithm)
+// TODO: Inline to optimize away function call (since only used once)
+void ssub(int *e, const int *a, const int *b, const int *c, const int *d, 
+	  int dim) {
+    for (int i = 0; i < dim; i++)
+	for (int j = 0; j < dim; j++)
+	    MELT(e, dim, i, j) = MELT(a, dim, i, j) + MELT(b, dim, i, j)
+		- MELT(c, dim, i, j) - MELT(d, dim, i, j);
+}
+
+// strassen(c, a, b, dim)
+//   Multiplication of square matrices using the Strassen algorithm (a x b = c)
+//   Comments based off of the following matrix multiplication:
+//    -    -    -   -       -                - 
+//   | A  B |  | E F |  =  | AE + BG  AF + BH | 
+//   | C  D |  | G H |     | CE + DG  CF + DH |
+//    -    -    -   -       -                -
+void strassen(int *c, const int *a, const int *b, int dim) {
+    if (dim == CROSSOVER)
+	square_matrix_multiply(c, a, b, dim);
+    else {
+	// cutting into submatrices
+	int dim_half = dim >> 2;
+
+	// sub-matrix size
+	int subsize = dim_half * dim_half;
+
+	// work matrix (for P1 through P7 - extra for work)
+	int *p = (int *) malloc(sizeof(int) * subsize * 8);
+
+	// First calculate P7, P6, & P5 due to double operations (double space)
+	// P7
+	msub(&p[0], &a[0], &a[2 * subsize], dim_half); // A - C
+	madd(&p[subsize], &b[0], &b[subsize], dim_half); // E + F
+	strassen(&p[6 * subsize], &p[0], &p[subsize], dim_half); // (A - C)(E + F)
+
+	// P6
+	msub(&p[0], &a[subsize], &a[3 * subsize], dim_half); // B - D
+	madd(&p[subsize], &b[2 * subsize], &b[3 * subsize], dim_half); // G + H
+	strassen(&p[5 * subsize], &p[0], &p[subsize], dim_half); // (B - D)(G + H)
+
+	// P5
+	madd(&p[0], &a[0], &a[3 * subsize], dim_half); // A + D
+	madd(&p[subsize], &b[0], &b[3 * subsize], dim_half); // E + H
+	strassen(&p[4 * subsize], &p[0], &p[subsize], dim_half); // (A + D)(E + H)
+	     
+	// P4
+	msub(&p[0], &b[3 * subsize], &b[0], dim_half); // G - E
+	strassen(&p[3 * subsize], &a[3 * subsize], &p[0], dim_half); // D(G - E)
+
+	// P3
+	madd(&p[0], &a[2 * subsize], &a[3 * subsize], dim_half); // C + D
+	strassen(&p[2 * subsize], &p[0], &b[0], dim_half); // (C + D)E
+
+	// P2
+	madd(&p[0], &a[0], &a[subsize], dim_half); // A + B
+	strassen(&p[subsize], &p[0], &b[3 * subsize], dim_half); // (A + B)H
+	
+	// P1
+	msub(&p[7 * subsize], &b[subsize], &b[3 * subsize], dim_half); // F - H
+	strassen(&p[0], &a[0], &p[7 * subsize], dim_half); // A(F - H)
+
+	// Calculate new matrix
+	sadd(&c[0], &p[4 * subsize], &p[3 * subsize], &p[subsize], 
+	     &p[5 * subsize], dim_half); // AE + BG  (P5 + P4 - P2 + P6)
+	madd(&c[subsize], &p[0], &p[subsize], dim_half); // AF + BH  (P1 + P2)
+	madd(&c[2 * subsize], &p[2 * subsize], &p[3 * subsize], dim_half); // CE + DG  (P3 + P4)
+	ssub(&c[3 * subsize], &p[4 * subsize], &p[0], &p[2 * subsize], 
+	     &p[6 * subsize], dim_half); // CF + DH  (P5 + P1 - P3 + P7)
+
+	// deallocate work matrix
+	free(p);
+    }
 }
 
 int main(int argc, char **argv) {
@@ -53,9 +164,6 @@ int main(int argc, char **argv) {
     fclose(fp);
 
     int *mc = (int *) malloc(sizeof(int) * dim * dim);
-    for (i = 0; i < dim; i++)
-        for (j = 0; j < dim; j++)
-            MELT(mc, dim, i, j) = 0;
         
     square_matrix_multiply(mc, ma, mb, dim);
 
