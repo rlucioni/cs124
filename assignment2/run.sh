604140b92ae7dedac9d9be709b128bc6bf03bce2
Renzo Lucioni <renzolucioni@gmail.com>
2013-03-30 20:52:51 -0400

    rename test_script to run and modifications for data generation

---
+++ assignment2/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# usage: ./run.sh [dimension] [filename]
+
+echo "TESTING CUTOFF VALUES ON MATRICES IN $2 OF DIMENSION $1..."
+for ((i=0; i<=200; i++))
+do
+    #echo -n $i
+    time ./strassen 0 $1 $2 $i
+done
