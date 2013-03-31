d7f5c2e58768b8c83411f3876eaf0bbcdd940389
Renzo Lucioni <renzolucioni@gmail.com>
2013-03-30 21:47:37 -0400

    graphs updated

--- assignment2/run.sh
+++ assignment2/run.sh
@@ -3,7 +3,7 @@
 # usage: ./run.sh [dimension] [filename]
 
 echo "TESTING CUTOFF VALUES ON MATRICES IN $2 OF DIMENSION $1..."
-for ((i=0; i<=200; i++))
+for ((i=1; i<=200; i++))
 do
     #echo -n $i
     time ./strassen 0 $1 $2 $i
