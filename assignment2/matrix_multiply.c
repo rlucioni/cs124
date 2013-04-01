999b9aef2085585c3975c637de9c8cd6011b7e38
Renzo Lucioni <renzolucioni@gmail.com>
2013-04-01 16:26:08 -0400

    tex additions, code edits for style

--- assignment2/matrix_multiply.c
+++ assignment2/matrix_multiply.c
@@ -3,6 +3,9 @@
 
 #include "matrix_multiply.h"
 
+// Determined experimentally, as described in the writeup
+#define CROSSOVER 32
+
 // we leave out dim and dim_orig to avoid repitition (they're the same for all matrices in a
 //   function - struct
 typedef struct {
@@ -15,8 +18,7 @@ typedef struct {
 // accesses the element in "matrix" of dimension "dim" at "row" and "column"
 #define MELT(m, row, col) (m).matrix[((row) + (m).row_off) * (m).dim_real + ((col) + (m).col_off)]
 
-int32_t crossover;
-
+//int32_t crossover;
 
 // mrow_off = Matrix m row offset
 // mcol_off = Matrix m col offset
@@ -36,10 +38,10 @@ void square_matrix_multiply(matrix c, const matrix a, const matrix b, int32_t di
             MELT(c, i, j) = 0;
     for (int32_t i = 0; i < dim; i++)
         for (int32_t k = 0; k < dim; k++) {
-	    int32_t a_ik = MELT(a, i, k);
+            int32_t a_ik = MELT(a, i, k);
             for (int32_t j = 0; j < dim; j++)
                 MELT(c, i, j) += a_ik * MELT(b, k, j);
-	}
+        }
 }
 
 // madd(c, a, b, dim)
@@ -63,11 +65,11 @@ void msub(matrix c, const matrix a, const matrix b, int32_t dim) {
 //   redundant loops (used for AE + BG in the Strassen Algorithm)
 // TODO: Inline to optimize away function call (since only used once)
 void sadd(matrix e, const matrix a, const matrix b, const matrix c, const matrix d, 
-	  int32_t dim) {
+      int32_t dim) {
     for (int32_t i = 0; i < dim; i++)
         for (int32_t j = 0; j < dim; j++)
-	    MELT(e, i, j) = MELT(a, i, j) + MELT(b, i, j)
-		- MELT(c, i, j) + MELT(d, i, j);
+            MELT(e, i, j) = MELT(a, i, j) + MELT(b, i, j)
+            - MELT(c, i, j) + MELT(d, i, j);
 }
 
 // ssub(e, a, b, c, d, dim)
@@ -75,11 +77,11 @@ void sadd(matrix e, const matrix a, const matrix b, const matrix c, const matrix
 //   redundant loops (used for CF + DH in the Strassen Algorithm)
 // TODO: Inline to optimize away function call (since only used once)
 void ssub(matrix e, const matrix a, const matrix b, const matrix c, const matrix d, 
-	  int32_t dim) {
+      int32_t dim) {
     for (int32_t i = 0; i < dim; i++)
-	for (int32_t j = 0; j < dim; j++)
-	    MELT(e, i, j) = MELT(a, i, j) + MELT(b, i, j)
-		- MELT(c, i, j) - MELT(d, i, j);
+        for (int32_t j = 0; j < dim; j++)
+            MELT(e, i, j) = MELT(a, i, j) + MELT(b, i, j)
+            - MELT(c, i, j) - MELT(d, i, j);
 }
 
 matrix init_matrix(matrix o, int32_t row_off, int32_t col_off) {
@@ -98,11 +100,12 @@ matrix init_matrix(matrix o, int32_t row_off, int32_t col_off) {
 //   | C  D |  | G H |     | CE + DG  CF + DH |
 //    -    -    -   -       -                -
 void strassen(matrix c, const matrix a, const matrix b, int32_t dim) {
-    if (dim <= crossover)
+    //if (dim <= crossover)
+    if (dim <= CROSSOVER)
         square_matrix_multiply(c, a, b, dim);
     else {
-	// cutting into submatrices
-	int32_t dim_half = dim >> 1;
+        // cutting into submatrices
+        int32_t dim_half = dim >> 1;
 
         matrix a1 = init_matrix(a, 0, 0);
         matrix a2 = init_matrix(a, 0, dim_half);
@@ -119,7 +122,7 @@ void strassen(matrix c, const matrix a, const matrix b, int32_t dim) {
         matrix c3 = init_matrix(c, dim_half, 0);
         matrix c4 = init_matrix(c, dim_half, dim_half);
 
-	// work matrix (for P1 through P7 - extra for work)
+        // work matrix (for P1 through P7 - extra for work)
         int32_t subsize = dim_half * dim_half;
 
         int32_t *p = (int32_t *) malloc(sizeof(int32_t) * subsize * 8);
@@ -139,63 +142,65 @@ void strassen(matrix c, const matrix a, const matrix b, int32_t dim) {
         matrix p8 = {.matrix = (&p[7 * subsize]), .row_off = 0, .col_off = 0,
                      .dim_real = dim_half};
 
-	// First calculate P7, P6, & P5 due to double operations (double space)
-	// P7
-	msub(p1, a1, a3, dim_half); // A - C
-	madd(p2, b1, b2, dim_half); // E + F
+        // First calculate P7, P6, & P5 due to double operations (double space)
+        // P7
+        msub(p1, a1, a3, dim_half); // A - C
+        madd(p2, b1, b2, dim_half); // E + F
         strassen(p7, p1, p2, dim_half); // (A - C)(E + F)
         
-	// P6
-	msub(p1, a2, a4, dim_half); // B - D
-	madd(p2, b3, b4, dim_half); // G + H
+        // P6
+        msub(p1, a2, a4, dim_half); // B - D
+        madd(p2, b3, b4, dim_half); // G + H
         strassen(p6, p1, p2, dim_half); // (B - D)(G + H)
 
-	// P5
-	madd(p1, a1, a4, dim_half); // A + D
-	madd(p2, b1, b4, dim_half); // E + H
+        // P5
+        madd(p1, a1, a4, dim_half); // A + D
+        madd(p2, b1, b4, dim_half); // E + H
         strassen(p5, p1, p2, dim_half); // (A + D)(E + H)
-	     
-	// P4
-	msub(p1, b3, b1, dim_half); // G - E
+
+        // P4
+        msub(p1, b3, b1, dim_half); // G - E
         strassen(p4, a4, p1, dim_half); // D(G - E)
 
-	// P3
-	madd(p1, a3, a4, dim_half); // C + D
+        // P3
+        madd(p1, a3, a4, dim_half); // C + D
         strassen(p3, p1, b1, dim_half); // (C + D)E
 
-	// P2
-	madd(p1, a1, a2, dim_half); // A + B
+        // P2
+        madd(p1, a1, a2, dim_half); // A + B
         strassen(p2, p1, b4, dim_half); // (A + B)H
-	
-	// P1
-	msub(p8, b2, b4, dim_half); // F - H
+
+        // P1
+        msub(p8, b2, b4, dim_half); // F - H
         strassen(p1, a1, p8, dim_half); // A(F - H)
 
-	// Calculate new matrix
-	sadd(c1, p5, p4, p2, p6, dim_half); // AE + BG  (P5 + P4 - P2 + P6)
-	madd(c2, p1, p2, dim_half); // AF + BH  (P1 + P2)
-	madd(c3, p3, p4, dim_half); // CE + DG  (P3 + P4)
-	ssub(c4, p5, p1, p3, p7, dim_half); // CF + DH  (P5 + P1 - P3 + P7)
+        // Calculate new matrix
+        sadd(c1, p5, p4, p2, p6, dim_half); // AE + BG  (P5 + P4 - P2 + P6)
+        madd(c2, p1, p2, dim_half); // AF + BH  (P1 + P2)
+        madd(c3, p3, p4, dim_half); // CE + DG  (P3 + P4)
+        ssub(c4, p5, p1, p3, p7, dim_half); // CF + DH  (P5 + P1 - P3 + P7)
 
-	// deallocate work matrix
-	free(p);
+        // deallocate work matrix
+        free(p);
     }
 }
 
 int main(int argc, char **argv) {
     // process command line arguments
-    /*if (argc != 4) {
+    if (argc != 4) {
         fprintf(stderr, "usage: %s flag dimension inputfile\n", argv[0]);
         exit(1);
-    }*/
-    if (argc != 5) {
+    }
+    
+    /*if (argc != 5) {
         fprintf(stderr, "usage: %s flag dimension inputfile crossover\n", argv[0]);
         exit(1);
-    }
+    }*/
+
     int32_t flag = atoi(argv[1]);
     int32_t dim = atoi(argv[2]);
     char* inputfile = argv[3];
-    crossover = atoi(argv[4]);
+    //crossover = atoi(argv[4]);
     
     assert(dim > 0);
 
@@ -214,7 +219,7 @@ int main(int argc, char **argv) {
     if ((dim & (dim - 1)) != 0) {
     // Bit Twiddling Hack for finding next highest power of 2
     //   http://graphics.stanford.edu/~seander/bithacks.html
-	//   -expects 32 bit architecture
+    //   -expects 32 bit architecture
         dim_pad--;
         dim_pad |= dim_pad >> 1;
         dim_pad |= dim_pad >> 2;
@@ -259,17 +264,16 @@ int main(int argc, char **argv) {
                  .row_off = 0, .col_off = 0, .dim_real = dim_pad};
     
     // track CPU time for the modified Strassen's
-    clock_t start = clock();
-    
+    clock_t start = clock();    
     strassen(mc, ma, mb, dim_pad);
-    
     int runtime = clock() - start;
 
-    // time in microseconds
+    // use flag 1 to get time in microseconds
     if (flag == 1) {
         int usec = (runtime * 1000000) / CLOCKS_PER_SEC;
         printf("%d\n", usec);
     }
+    // use flag 0 (or other flags) to get diagonal elements
     else {
         // print diagonal elements - does not read padding
         for (i = 0; i < dim; i++)
