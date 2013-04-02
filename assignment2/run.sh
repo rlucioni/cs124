1e1baf39429a5f707bd5915a01fc35a35bbd7951
Renzo Lucioni <renzolucioni@gmail.com>
2013-04-01 20:57:58 -0400

    sync for tex and graphs

--- assignment2/run.sh
+++ assignment2/run.sh
@@ -3,8 +3,8 @@
 # usage: ./run.sh [dimension] [filename]
 #count=0
 echo "TESTING CUTOFF VALUES ON MATRICES IN $2 OF DIMENSION $1..."
-for ((i=1; i<=1000; i=$i+2))
-#for ((i=1; i<=500; i++))
+#for ((i=1; i<=1000; i=$i+2))
+for ((i=1; i<=500; i++))
 do
     #count=$(($count+1))
     #echo $i
